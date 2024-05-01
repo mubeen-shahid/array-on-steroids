0e0afd235d4336ae845caac7bafa24895b3cf0bb
XeTute <154016609+N0CTRON@users.noreply.github.com>
2024-05-01 17:04:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
 #include "aos.hpp"
+
+// DEMO OF AOS / Array On Steroids.
 
 int main()
 {
-	AOS<AOS<int>> myArray(10);
-	myArray.data[0].init(10);
+	AOS<AOS<int>> myArray(10); //create two-dimensional array with the data-type of int
+
+	{ //bracets cuz var "aSize" is tmp 
+		const size_t aSize = myArray.getSize(); // aSize = array size
+		for (size_t row = 0; row < aSize; ++row) //init each element
+		{
+			myArray[row].init(aSize);
+			for (size_t column = 0; column < aSize; ++column) 
+			{
+				myArray[row][column] = row * column; //assign unique value
+				std::cout << myArray[row][column] << " | "; // std::cout << myArray.data[row].data[column] would also work, operator was overloaded
+			}
+			std::cout << "\n--------------------\n";
+		}
+	}
 
 	return 0;
 }
