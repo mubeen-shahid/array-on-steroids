c24d964873f50a3fba8f39eaa27392f60be55fee
XeTute <154016609+N0CTRON@users.noreply.github.com>
2024-05-02 21:06:24 +0200

    Better comments on main.cpp

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@
 
 int main()
 {
-	AOS<AOS<int>> myArray(5); //create two-dimensional array with the data-type of int
-	//myArray.resize(10); //arrays can be resized. Appearently doesn't work. Why? Because I told ChatGPT to write the function.
+	AOS<AOS<int>> myArray(3); //create two-dimensional array with the data-type of int
 
 	{ //bracets cuz var "aSize" is tmp 
-		const size_t aSize = myArray.getSize(); // aSize = array size
+		const size_t aSize = myArray.size(); // aSize = array size
 		for (size_t row = 0; row < aSize; ++row) //init each element
 		{
-			myArray[row].init(aSize);
+			myArray[row].init(aSize, 9);
+			//myArray[row].init(aSize); <-- This line would automatically set every value to "NULL" / 0. Nice, right? :3
+
 			for (size_t column = 0; column < aSize; ++column) 
 			{
-				myArray[row][column] = row * column; //assign unique value
+				myArray[row][column] = row * column; //assign unique value. It would not crash, because AOS auto-inits to zero if not value was provided
 				std::cout << myArray[row][column] << " | "; // std::cout << myArray.data[row].data[column] would also work, operator was overloaded
 			}
-			std::cout << "\n--------------------\n";
+			std::cout << "\n-----------\n";
 		}
 	}
 	
-	myArray.suicide();
+	myArray.suicide(); //Let the array kill itself. This prevents memory leaks
 
 	return 0;
 }
