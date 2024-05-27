162048a6b6490b9db50cc1b62ad71090e60e2c12
XeTute <154016609+N0CTRON@users.noreply.github.com>
2024-05-27 17:22:02 +0200

    Fixed errors

--- aos.hpp
+++ aos.hpp
@@ -13,14 +13,14 @@ public:
 	void init(std::uint64_t nSize)
 	{
 		vsize = nSize;
-		data = (arrType*)malloc((constexpr)sizeof(arrType) * nSize);
+		data = (arrType*)malloc((std::uint64_t)sizeof(arrType) * nSize);
 		for (std::uint64_t i = 0; i < nSize; ++i) this->data[i] = arrType(NULL);
 	}
 
 	void init(std::uint64_t nSize, arrType defaultVal)
 	{
 		vsize = nSize;
-		data = (arrType*)malloc((constexpr)sizeof(arrType) * nSize);
+		data = (arrType*)malloc((std::uint64_t)sizeof(arrType) * nSize);
 		for (std::uint64_t i = 0; i < nSize; ++i) this->data[i] = (arrType)defaultVal;
 	}
 
@@ -50,12 +50,11 @@ public:
 	AOS(std::initializer_list<arrType> list)
 	{
 		vsize = list.size();
-		data = (arrType*)malloc((constexpr)sizeof(arrType) * vsize);
+		data = (arrType*)malloc(sizeof(arrType) * vsize);
 		std::copy(list.begin(), list.end(), data);
 	}
 	AOS(std::uint64_t nSize) { init(nSize); };
 	AOS(std::uint64_t nSize, arrType defaultVal) { init(nSize, defaultVal); };
-	AOS() : AOS({}) {}
 	AOS() {};
 	~AOS() { std::thread(suicide); } //<-- Threading was tested, about 1.9 times faster than calling "suicide" from main thread
 
@@ -63,7 +62,7 @@ public:
 	AOS& operator=(std::initializer_list<arrType> list)
 	{
 		vsize = list.size();
-		data = (arrType*)malloc((constexpr)sizeof(arrType) * vsize);
+		data = (arrType*)malloc((std::uint64_t)sizeof(arrType) * vsize);
 		std::copy(list.begin(), list.end(), data);
 		return *this;
 	}
